814c103ba7dca117845a9537146ac103f96e88e5
dianamartine <diana_martine@outlook.com>
2025-03-03 00:14:40 -0300

    feat: finish the project

--- BeTalent/be-talent/src/services/api.js
+++
@@ -1,9 +0,0 @@
-const baseUrl = 'http://localhost:3001';
-
-const getEmployees = async () => {
-    const response = await fetch(`${baseUrl}/employees`);
-    const data = await response.json();
-    return data;
-}
-
-export default getEmployees;
--- BeTalent/be-talent/src/utils/formatDate.js
+++
@@ -1,9 +0,0 @@
-function formatDate(date) {
-    if (!date) {
-        return '';
-    }
-    
-    return new Date(date).toLocaleDateString('pt-br');
-}
-
-export default formatDate;
